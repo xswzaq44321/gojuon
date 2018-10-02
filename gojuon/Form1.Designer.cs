a57425ed386ba84a3c3e8e2dac7d053c02360cb2
Weber <xswzaq44321@gmail.com>
2018-10-03 02:35:53 +0800

    first commit

---
+++ gojuon/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace gojuon
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1_qus = new System.Windows.Forms.Label();
+			this.textBox_ans = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// label1_qus
+			// 
+			this.label1_qus.AutoSize = true;
+			this.label1_qus.Font = new System.Drawing.Font("UD Digi Kyokasho NK-R", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.label1_qus.Location = new System.Drawing.Point(65, 52);
+			this.label1_qus.Name = "label1_qus";
+			this.label1_qus.Size = new System.Drawing.Size(88, 64);
+			this.label1_qus.TabIndex = 0;
+			this.label1_qus.Text = "あ";
+			this.label1_qus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// textBox_ans
+			// 
+			this.textBox_ans.Font = new System.Drawing.Font("UD Digi Kyokasho NK-R", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.textBox_ans.Location = new System.Drawing.Point(66, 173);
+			this.textBox_ans.Name = "textBox_ans";
+			this.textBox_ans.Size = new System.Drawing.Size(87, 55);
+			this.textBox_ans.TabIndex = 1;
+			this.textBox_ans.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_ans_KeyDown);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.textBox_ans);
+			this.Controls.Add(this.label1_qus);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Resize += new System.EventHandler(this.Form1_Resize);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1_qus;
+		private System.Windows.Forms.TextBox textBox_ans;
+	}
+}
+
---
+++ gojuon/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gojuon
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+			now = rand.Next(46);
+			label1_qus.Text = char.ToString(gojuin[now]);
+
+			Control control = (Control)this;
+
+			textBox_ans.Location = new Point((control.Size.Width - textBox_ans.Size.Width) / 2,
+											(control.Size.Height - textBox_ans.Size.Height) / 4 * 3);
+			label1_qus.Location = new Point((control.Size.Width - label1_qus.Size.Width) / 2,
+											(control.Size.Height - label1_qus.Size.Height) / 4);
+		}
+
+		Random rand = new Random();
+		int now;
+		string gojuin = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん";
+		string[] pronounce = new string[46] {
+			"a", "i", "u", "e", "o",
+			"ka", "ki", "ku", "ke", "ko",
+			"sa", "shi", "su", "se", "so",
+			"ta", "chi", "tsu", "te", "to",
+			"na", "ni", "nu", "ne", "no",
+			"ha", "hi", "fu", "he", "ho",
+			"ma", "mi", "mu", "me", "mo",
+			"ya", "yu", "yo",
+			"ra", "ri", "ru", "re", "ro",
+			"wa", "wo",
+			"n"
+		};
+
+		private void textBox_ans_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Enter)
+			{
+				System.Diagnostics.Debug.WriteLine("[" + textBox_ans.Text + "]");
+				if (textBox_ans.Text == pronounce[now])
+				{
+					textBox_ans.Text = "";
+					now = rand.Next(46);
+					label1_qus.Text = char.ToString(gojuin[now]);
+				}
+			}
+		}
+
+		private void Form1_Resize(object sender, EventArgs e)
+		{
+			Control control = (Control)sender;
+			
+			textBox_ans.Location = new Point((control.Size.Width - textBox_ans.Size.Width) / 2,
+											(control.Size.Height- textBox_ans.Size.Height) / 4 * 3);
+			label1_qus.Location = new Point((control.Size.Width - label1_qus.Size.Width) / 2,
+											(control.Size.Height - label1_qus.Size.Height) / 4);
+		}
+	}
+}
